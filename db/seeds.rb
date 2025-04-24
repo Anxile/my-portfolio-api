977d00a340faf6b65bf0bfdd3359cce9d6609637
Anxile <onebox.chen@outlook.com>
2025-04-24 15:21:23 -0400

    modified seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
 User.destroy_all
 
 fake_users = [
-  { email: 'user1@example.com', password: 'password1' },
+  { email: 'chen61@mcmaster.ca', password: '123456' },
   { email: 'user2@example.com', password: 'password2' },
   { email: 'user3@example.com', password: 'password3' },
   { email: 'user4@example.com', password: 'password4' },
